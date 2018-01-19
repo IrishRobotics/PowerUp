8336c5d59ade7c2fcfaacbf950b89f146822fc98
Sean <34431466+Smwitte27@users.noreply.github.com>
2018-01-18 19:47:43 -0600

    Update RobotMap.java

--- src/org/usfirst/frc/team2606/robot/RobotMap.java
+++ src/org/usfirst/frc/team2606/robot/RobotMap.java
@@ -14,13 +14,39 @@ package org.usfirst.frc.team2606.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
+	    public static SpeedController driveTrain_SubsystemFrontLeft;
+	    public static SpeedController driveTrain_SubsystemRearLeft;
+	    public static SpeedControllerGroup driveTrain_SubsystemLeftDrive;
+	    public static SpeedController driveTrain_SubsystemFrontRight;
+	    public static SpeedController driveTrain_SubsystemRearRight;
+	    public static SpeedControllerGroup driveTrain_SubsystemRightDrive;
+	    public static DifferentialDrive driveTrain_SubsystemDifferentialDrive1;
+	    public static SpeedController wheelIntake_SubsystemLeftWheel;
+	    public static SpeedController wheelIntake_SubsystemRightWheel;
+	    public static SpeedController lifter_SubsystemSpeedController1;
 
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	public static void init () {
+		
+    driveTrain_SubsystemFrontLeft = new Talon(0);
+    driveTrain_SubsystemFrontLeft.setInverted(false);
+    driveTrain_SubsystemRearLeft = new Talon(1);
+    driveTrain_SubsystemRearLeft.setInverted(false);
+    driveTrain_SubsystemLeftDrive = new SpeedControllerGroup(driveTrain_SubsystemFrontLeft, driveTrain_SubsystemRearLeft  );
+    
+    driveTrain_SubsystemFrontRight = new Talon(2);
+    driveTrain_SubsystemFrontRight.setInverted(false);
+    driveTrain_SubsystemRearRight = new Talon(3);
+    driveTrain_SubsystemRearRight.setInverted(false);
+    driveTrain_SubsystemRightDrive = new SpeedControllerGroup(driveTrain_SubsystemFrontRight, driveTrain_SubsystemRearRight  );
+    
+    driveTrain_SubsystemDifferentialDrive1 = new DifferentialDrive(driveTrain_SubsystemLeftDrive, driveTrain_SubsystemRightDrive);
+    driveTrain_SubsystemDifferentialDrive1.setSafetyEnabled(true);
+    driveTrain_SubsystemDifferentialDrive1.setExpiration(0.1);
+    driveTrain_SubsystemDifferentialDrive1.setMaxOutput(1.0);
+    
+    wheelIntake_SubsystemLeftWheel = new Spark(4);
+    wheelIntake_SubsystemLeftWheel.setInverted(false);
+    wheelIntake_SubsystemRightWheel = new Spark(5);
+    wheelIntake_SubsystemRightWheel.setInverted(true);
+	}
 }
