4556cca644321fcb2fa3f1a3305d4105d1d81b1a
Sean <seanmwitte@gmail.com>
2018-01-20 21:53:53 -0600

    Made Public speed controllers Private

--- src/org/usfirst/frc/team2606/robot/RobotMap.java
+++ src/org/usfirst/frc/team2606/robot/RobotMap.java
@@ -17,10 +17,10 @@ import edu.wpi.first.wpilibj.*;
  * floating around.
  */
 public class RobotMap {
-    public static SpeedController FRONT_LEFT_MOTOR = new Talon(1);
-    public static SpeedController BACK_LEFT_MOTOR = new Talon(0);
-    public static SpeedController FRONT_RIGHT_MOTOR = new Spark(3);
-    public static SpeedController BACK_RIGHT_MOTOR = new Talon(2);
+    private static SpeedController FRONT_LEFT_MOTOR = new Talon(1);
+    private static SpeedController BACK_LEFT_MOTOR = new Talon(0);
+    private static SpeedController FRONT_RIGHT_MOTOR = new Spark(3);
+    private static SpeedController BACK_RIGHT_MOTOR = new Talon(2);
     public static SpeedControllerGroup LEFT_TANK_DRIVE = new SpeedControllerGroup(FRONT_LEFT_MOTOR, BACK_LEFT_MOTOR);
     public static SpeedControllerGroup RIGHT_TANK_DRIVE = new SpeedControllerGroup(FRONT_RIGHT_MOTOR, BACK_RIGHT_MOTOR);
 
