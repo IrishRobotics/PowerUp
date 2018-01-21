d6513bd2a5316ed84ba13977a74f9bed68aa2308
F4th0m0r9 <jps7838752000@gmail.com>
2018-01-21 13:57:38 -0600

    Update OI.java

--- src/org/usfirst/frc/team2606/robot/OI.java
+++ src/org/usfirst/frc/team2606/robot/OI.java
@@ -55,12 +55,8 @@ public class OI {
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
 
-    public Joystick getLeftJoystick() {
-        return leftJoystick;
-    }
-
-    public Joystick getRightJoystick() {
-        return rightJoystick;
+    public Joystick getXboxController() {
+        return XboxController;
     }
 
 }
